4d746593453a9266da070a0125ca1076d7e51fc9
Sam Keshmiri <sam.k@aave.com>
2024-09-03 19:18:52 +0100

    nest function

--- BinarySearch/Easy/SearchInsert.ts
+++ BinarySearch/Easy/SearchInsert.ts
@@ -1,28 +1,28 @@
 function searchInsert(nums: number[], target: number): number {
+  function search(
+    nums: number[],
+    target: number,
+    startIndex: number,
+    endIndex: number
+  ): number {
+    if (startIndex > endIndex) {
+      return startIndex;
+    }
+
+    const currentIndex = Math.floor((startIndex + endIndex) / 2);
+
+    if (nums[currentIndex] === target) {
+      return currentIndex;
+    }
+
+    if (nums[currentIndex] > target) {
+      return search(nums, target, startIndex, currentIndex - 1);
+    } else {
+      return search(nums, target, currentIndex + 1, endIndex);
+    }
+  }
+
   return search(nums, target, 0, nums.length - 1);
-}
-
-function search(
-  nums: number[],
-  target: number,
-  startIndex: number,
-  endIndex: number
-): number {
-  if (startIndex > endIndex) {
-    return startIndex;
-  }
-
-  const currentIndex = Math.floor((startIndex + endIndex) / 2);
-
-  if (nums[currentIndex] === target) {
-    return currentIndex;
-  }
-
-  if (nums[currentIndex] > target) {
-    return search(nums, target, startIndex, currentIndex - 1);
-  } else {
-    return search(nums, target, currentIndex + 1, endIndex);
-  }
 }
 
 console.log(searchInsert([1, 2, 3], 4));
