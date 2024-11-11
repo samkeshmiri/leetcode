2ca0090e461ed3b030a84f2d00a135c79521ff17
Sam Keshmiri <sam.k@aave.com>
2024-11-11 22:00:31 +0000

    daily: prime subtraction operation

---
+++ Arrays/Medium/PrimeSubtractionOperation.ts
@@ -0,0 +1,42 @@
+// 2601
+function primeSubOperation(nums: number[]): boolean {
+  let previous = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const current = nums[i];
+    if (previous >= current) {
+      return false;
+    }
+
+    const highestPrime = findHighestPrime(current - previous);
+    previous = current - highestPrime;
+  }
+  return true;
+}
+
+function findHighestPrime(num: number) {
+  const primes = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+    157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
+    239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+    331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419,
+    421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
+    509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607,
+    613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701,
+    709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811,
+    821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911,
+    919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+  ];
+
+  let highest = 0;
+  for (const prime of primes) {
+    if (prime >= num) {
+      return highest;
+    }
+    highest = prime;
+  }
+
+  return highest;
+}
+
+console.log(primeSubOperation([4, 9, 6, 10])); // true
