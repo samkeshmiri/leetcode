b2a05eb840ca09d9fb8046e950b570150b025b5b
Sam Keshmiri <sam.k@aave.com>
2025-01-09 19:45:40 +0000

    55. Jump Game

---
+++ Arrays/Medium/JumpGame.ts
@@ -0,0 +1,44 @@
+// 55. Jump Game
+
+function canJump(nums: number[]): boolean {
+  let farthest = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > farthest) {
+      // index is unreachable
+      return false;
+    }
+
+    farthest = Math.max(farthest, i + nums[i]);
+
+    if (farthest >= nums.length - 1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// greedy solution
+function canJumpGreedy(nums: number[]): boolean {
+  let goalPostIndex = nums.length - 1;
+
+  // go in reverse and move the goalpost if the previous index can get us to the current goalpost
+  for (let i = nums.length - 1; i >= 0; i--) {
+    // can the current index and value take me past the goalpost?
+    if (i + nums[i] >= goalPostIndex) {
+      goalPostIndex = i;
+    }
+  }
+
+  return goalPostIndex === 0;
+}
+
+canJumpGreedy;
+
+// console.log(canJump([2, 3, 1, 1, 4])); // true;
+// console.log(canJump([3, 2, 1, 0, 4])); // false;
+console.log(canJumpGreedy([3, 2, 1, 0, 4])); // false;
+// console.log(canJumpGreedy([2, 3, 1, 1, 4])); // true;
+// console.log(canJump([2, 0])); // true
+// console.log(canJump([2, 5, 0, 0])); // true
