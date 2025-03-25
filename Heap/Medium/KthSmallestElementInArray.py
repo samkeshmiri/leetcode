e548df343cc96d48198db2dbafab23b45dec13c5
Sam Keshmiri <sam.k@aave.com>
2025-03-25 16:06:52 +0000

    215. Kth Largest Element in an Array: QuickSelect

--- Heap/Medium/KthSmallestElementInArray.py
+++ Heap/Medium/KthSmallestElementInArray.py
@@ -1,26 +1,9 @@
-# 215. Kth Largest Element in an Array
 
-import heapq
 from typing import List
 
 
-# heap solution
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heapq.heapify(nums)
-        return heapq.nlargest(nums, k)[k]
-
-# quick select
-
-
-class Solution:
-    def findKthLargestQuickSelect(self, nums: List[int], k: int) -> int:
-        # we're converting from k-th largest to k-th smallest (0-based), since QuickSelect is better suited for selecting by index in ascending order.
-        # the kth element that we are interested in basically eliminates this many elements
-        # so by the time our partition index reaches k, we have gone through the elements that we are not interested in
-        # we pass in the k = 2 but that also means the 5th largest elemnent when len of nums = 7
-        k = len(nums) - k
-
+    def findKthSmalletsQuickSelect(self, nums: List[int], k: int) -> int:
         def quickSelect(l, r):
             # we can choose any index between l and r, we use rightmost value as our pivot
             pivot, partition_index = nums[r], l
@@ -42,13 +25,14 @@ class Solution:
             # if partition index is less than the kth element that we are interested, then our answer lies right of the partition index
             if k > partition_index:
                 return quickSelect(partition_index + 1, r)
-            # if the partition index is k then we have found our kth largest element
+            # if the partition index is k then we have found our kth smallest element
             else:
                 return nums[partition_index]  # kth participle
 
+        # subtract 1 to convert to 0-based index
+        k -= 1
         # start algorithm using entire array
         return quickSelect(0, len(nums) - 1)
 
 
-# print(Solution().findKthLargestQuickSelect([3, 2, 0, 5, 6, 1, 4], 2))  # 5
-print(Solution().findKthLargestQuickSelect([3, 2, 0, 5, 6, 1, 4], 5))  # 2
+print(Solution().findKthSmalletsQuickSelect([3, 2, 0, 5, 6, 1, 4], 2))
